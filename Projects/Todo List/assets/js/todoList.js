11ecfe2f1e2d8526f12c5badadcd2bef17f6ea2c
deletestvn <deletestvn@gmail.com>
2020-04-03 15:17:16 -0400

    Add JS functions to the To-Do List to mark completion, delete Todos and create new Todos

---
+++ Projects/Todo List/assets/js/todoList.js
@@ -0,0 +1,27 @@
+//Marking Completion
+function markCompletion(todo){
+	$(todo).toggleClass("todoCompleted");
+}
+//Deleting Todo
+function deleteTodo(todo){
+	$(todo).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+}
+//Creating Todo
+function createTodo(todo){
+	var todoText = $(todo).val();
+	$(todo).val("");
+	$("ul").append("<li><span>X</span> "+todoText+"</li>");
+}
+
+$("ul").on("click", "li", function(){markCompletion(this);});
+$("ul").on("click", "span", function(event){
+	deleteTodo(this);
+	event.stopPropagation;
+});
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		createTodo(this);
+	}
+});
