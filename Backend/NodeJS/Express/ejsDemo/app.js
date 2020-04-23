5f8639bf5bb7432bec4a494f631e7b09899fb9a4
deletestvn <deletestvn@gmail.com>
2020-04-22 20:42:19 -0400

    Add EJS 'conditionals and loops' contents with a job experience page

--- Backend/NodeJS/Express/ejsDemo/app.js
+++ Backend/NodeJS/Express/ejsDemo/app.js
@@ -4,6 +4,16 @@ app.get("/", function(req, res){
 	res.render("home.ejs");
 });
 
+app.get("/experience", function(req, res){
+	var experiences = [
+		{company: "Keywords Studios", title: "Funtionality QA"},
+		{company: "Intel Corporation", title: "Technique Intern"},
+		{company: "Loho MTL", title: "Social Media Specialist"}
+	];
+
+	res.render("experience.ejs", {experiences: experiences});
+});
+
 app.get("/projects/:name", function(req, res){
 	var projectName = req.params.name;
 	res.render("project.ejs", {projectName: projectName});
