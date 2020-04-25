294a98694b8163c930ecfa2451be7f8c591b4d83
deletestvn <deletestvn@gmail.com>
2020-04-24 20:39:35 -0400

    Create NookCamp - a animal-crossing tool for players

---
+++ Projects/Nook Camp/app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var ejs = require("ejs");
+
+var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.get("/villagers/", function(req, res){
+	var villagers= [
+		{name: "Ankha", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/5/51/Ankha.png?width=325"},
+		{name: "Bob", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/2/23/Bob_NewLeaf.png?width=325"},
+		{name: "Felicity", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/a/aa/Felicity.png?width=325"},
+		{name: "Kabuki", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/5/57/Kabuki.png?width=325"},
+		{name: "Katt", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/2/27/Katt_NewLeaf.png?width=325"}
+	/*	{name: "Kid Cat", image: ""}
+		{name: "Kiki", image: ""}
+		{name: "kitty", image: ""}
+		{name: "Lolly", image: ""}
+		{name: "Merry", image: ""}
+		{name: "Mitzi", image: ""}
+		{name: "Moe", image: ""}
+		{name: "Monique", image: ""}
+		{name: "Olivia", image: ""}
+		{name: "Punchy", image: ""}
+		{name: "Purrl", image: ""}
+		{name: "Raymond", image: ""}
+		{name: "Rosie", image: ""}
+		{name: "Rudy", image: ""}
+		{name: "Stinky", image: ""}
+		{name: "Tabby", image: ""}
+		{name: "Tangy", image: ""}
+		{name: "Tom", image: ""}*/
+	];
+	res.render("villagers", {villagers: villagers});
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("Server Connected!");
+});
