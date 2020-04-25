82c977db9af49d16c7507f882fbb9df0da86d622
deletestvn <deletestvn@gmail.com>
2020-04-24 23:50:08 -0400

    Add 'Create New Island' feature to NookCamp

--- Projects/Nook Camp/app.js
+++ Projects/Nook Camp/app.js
@@ -1,15 +1,19 @@
 var express = require("express");
 var ejs = require("ejs");
+var bodyParser = require("body-parser");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
-	res.render("index");
-});
+var islands = [
+		{name: "Verdun", owner: "ThiccGuy", zone: "EDT"},
+		{name: "utopia", owner: "huahua", zone: "EDT"},
+		{name: "mora mora", owner: "monmon", zone: "EDT"},
+		{name: "PurpleTaro", owner: "Mirrorsiri", zone: "EDT"}
+];
 
-app.get("/villagers/", function(req, res){
-	var villagers= [
+var villagers = [ 
 		{name: "Ankha", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/5/51/Ankha.png?width=325"},
 		{name: "Bob", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/2/23/Bob_NewLeaf.png?width=325"},
 		{name: "Felicity", image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/animal-crossing-new-horizons/a/aa/Felicity.png?width=325"},
@@ -33,8 +37,31 @@ app.get("/villagers/", function(req, res){
 		{name: "Tabby", image: ""}
 		{name: "Tangy", image: ""}
 		{name: "Tom", image: ""}*/
-	];
+];
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.get("/villagers/", function(req, res){	
 	res.render("villagers", {villagers: villagers});
+});
+
+app.get("/islands", function(req, res){
+	res.render("islands", {islands: islands});
+});
+
+app.get("/islands/new", function(req, res){
+	res.render("newIsland");
+});
+
+app.post("/islands", function(req, res){
+	var name = req.body.name;
+	var owner = req.body.owner;
+	var zone = req.body.zone;
+	var newIsland = {name: name, owner: owner, zone: zone};
+	islands.push(newIsland);
+	res.redirect("islands");
 });
 
 var port = process.env.PORT || 3000;
